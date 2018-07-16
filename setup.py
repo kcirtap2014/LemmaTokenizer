91ad4358772b1537a3d783401225abbac66caf36
Patrick Lee <patrick.lee@u-psud.fr>
2018-07-16 10:11:11 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='lemma_tokenizer',
+    author='Patrick lee',
+    author_email='lee.patrickmunseng@gmail.com',
+    packages=['lemma_tokenizer'],
+    description='lemma tokenizer class',
+    platforms='Linux, MacOSX',
+    url='github.com/kcirtap2014/lemma_tokenizer.git'
+)
